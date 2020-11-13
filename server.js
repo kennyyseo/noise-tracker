2eff1678defa0b95ba3eb160ebd3cd108d2e4400
Kenny Seo <kennyyseo@gmail.com>
2020-11-12 16:17:49 -0800

    Set up react project shell

--- server.js
+++ server.js
@@ -5,10 +5,19 @@ const logger = require('morgan');
 
 const app = express();
 
+require('dotenv').config();
+require('./config/database');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
+
+// Mount the auth middleware that processes JWTs
+
+
+// Put API routes here, before the "catch all" route
+
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
---
+++ src/pages/App/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
+
+export default App;
